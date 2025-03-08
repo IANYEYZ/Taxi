abcbed01ab6afabb4d617f461f560b38d78b53b5
IANYEYZ <Ian.ye01@outlook.com>
2025-03-08 16:47:07 +0800

    Update to test

---
+++ src/plugin/ui/tui.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include "../../taxi.hpp"
+
+class button {
+};
--- test.cpp
+++ test.cpp
@@ -12,7 +12,13 @@ surface img2 = surface({
     {Char(color("#FF00FF"), color("#ffffff"), "  "), Char(color("#FF00FF"), color("#ffffff"), "  "), Char(color("#FF00FF"), color("#ffffff"), "  ")},
     {Char(color("#FF00FF"), color("#ffffff"), "  "), Char(color("#FF00FF"), color("#ffffff"), "  "), Char(color("#FF00FF"), color("#ffffff"), "  ")}
 }, Char(color("#000000"), color("#ffffff"), "  "));
+surface img3 = surface({
+    {Char(color("#FFFF00"), color("#ffffff"), "  "), Char(color("#FFFF00"), color("#ffffff"), "  "), Char(color("#FFFF00"), color("#ffffff"), "  ")},
+    {Char(color("#FFFF00"), color("#ffffff"), "  "), Char(color("#FFFF00"), color("#ffffff"), "  "), Char(color("#FFFF00"), color("#ffffff"), "  ")},
+    {Char(color("#FFFF00"), color("#ffffff"), "  "), Char(color("#FFFF00"), color("#ffffff"), "  "), Char(color("#FFFF00"), color("#ffffff"), "  ")}
+}, Char(color("#000000"), color("#ffffff"), "  "));
 animatedSprite* s = new animatedSprite({img1, img1, img1, img1, img1, img2, img2, img2, img2, img2});
+animatedSprite* s1 = new animatedSprite({img2, img2, img2, img2, img2, img3, img3, img3, img3, img3});
 void spriteInit() {
 }
 void f(int cnt) {
@@ -26,6 +32,17 @@ void f(int cnt) {
         s -> move({1, 0});
     }
 }
+void g(int cnt) {
+    if (Input.isKeyPressed(VK_LEFT)) {
+        s1 -> move({0, -1});
+    } else if (Input.isKeyPressed(VK_RIGHT)) {
+        s1 -> move({0, 1});
+    } else if (Input.isKeyPressed(VK_UP)) {
+        s1 -> move({-1, 0});
+    } else if (Input.isKeyPressed(VK_DOWN)) {
+        s1 -> move({1, 0});
+    }
+}
 int main() {
     if (!init() || !initNode()) {
         printf("Error on initializing");
@@ -35,8 +52,10 @@ int main() {
     hideCursor();
     screen = makeSurface(100, 100, Char(color("#000000"), color("#ffffff"), "  "));
     s -> bind(spriteInit, f);
+    s1 -> bind(spriteInit, g);
     registerNode(s);
-    loopEvent.subscribe(f);
+    registerNode(s1);
+    // loopEvent.subscribe(f);
     cursorPosPreprocessor = y2;
     setFPS(60);
     return mainloop<diffRender>();
