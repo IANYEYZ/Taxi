95a35b86a9a484c583c44937aa9a824c528fbcb7
IANYEYZ <Ian.ye01@outlook.com>
2025-03-08 22:41:00 +0800

    Update UI plugin

--- src/plugin/ui/tui.hpp
+++ src/plugin/ui/tui.hpp
@@ -1,5 +1,25 @@
 #pragma once
 #include "../../taxi.hpp"
 
-class button {
+class UIElement {
+    public:
+    virtual void render(surface &sfc) = 0;
+};
+class button: UIElement {
+};
+class UISurface: public surface {
+    private:
+    vector<UIElement*> UIList_;
+    public:
+    void registerUIElement(UIElement* ui) {
+        UIList_.push_back(ui);
+    }
+    void renderAll(int cnt) {
+        for (auto i:UIList_) {
+            i -> render(*this);
+        }
+    }
+    void bind() {
+        loopEvent.subscribe([this](int cnt){ this -> renderAll(cnt); });
+    }
 };
